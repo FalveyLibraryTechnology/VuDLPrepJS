e1599d3ef8464bec0fafea090d0925f798d6f9dd
Demian Katz <demian.katz@villanova.edu>
2024-07-18 15:25:46 -0400

    Complete test.

--- client/components/edit/CopyPidButton.test.tsx
+++ client/components/edit/CopyPidButton.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import CopyPidButton from "./CopyPidButton";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 
 jest.mock(
     "@mui/icons-material/ContentCopy",
@@ -13,5 +15,13 @@ describe("CopyPidButton", () => {
     it("renders", async () => {
         const tree = renderer.create(<CopyPidButton pid="foo" />);
         expect(tree.toJSON()).toMatchSnapshot();
+    });
+
+    it("copies text to clipboard", async () => {
+        const user = userEvent.setup();
+        render(<CopyPidButton pid="foo" />);
+        const button = screen.getByText("Copy PID (foo) to clipboard");
+        await user.click(button);
+        expect(await navigator.clipboard.readText()).toEqual("foo");
     });
 });
