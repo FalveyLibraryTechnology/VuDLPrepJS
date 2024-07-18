651f74b5c9c78c9ce5ff30a3daa5f20989e90e83
Demian Katz <demian.katz@villanova.edu>
2024-07-18 13:37:10 -0400

    Simplify config.

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -1,23 +1,15 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import jsdoc from "eslint-plugin-jsdoc";
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+import tseslint from "typescript-eslint";
 
 export default [
     // jsdoc configuration, currently disabled until we're ready to enforce it:
     //jsdoc.configs['flat/recommended'],
-    ...compat.extends("plugin:@typescript-eslint/recommended"), {
+    // typescript configuration:
+    ...tseslint.configs.recommended,
+    // local settings:
+    {
         files: ["**/*.ts", "**/*.js", "**/*.jsx"],
 
         plugins: {
