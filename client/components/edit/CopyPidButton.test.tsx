363b7da87d35d37d237a5edb29bdc1542b2c3094
Demian Katz <demian.katz@villanova.edu>
2024-07-18 14:52:49 -0400

    Simplify snapshot.

--- client/components/edit/CopyPidButton.test.tsx
+++ client/components/edit/CopyPidButton.test.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import renderer from "react-test-renderer";
 import CopyPidButton from "./CopyPidButton";
 
+jest.mock(
+    "@mui/icons-material/ContentCopy",
+    () =>
+        ({ titleAccess }: { titleAccess: string }) =>
+            titleAccess,
+);
+
 describe("CopyPidButton", () => {
     it("renders", async () => {
         const tree = renderer.create(<CopyPidButton pid="foo" />);
