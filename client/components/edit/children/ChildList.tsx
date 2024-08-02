fce4236e5f8cb5dd043ad79dc5395e03034c4b7d
Demian Katz <demian.katz@villanova.edu>
2024-08-02 15:11:52 -0400

    Prevent server-side rendering errors.

--- client/components/edit/children/ChildList.tsx
+++ client/components/edit/children/ChildList.tsx
@@ -29,9 +29,12 @@ export const ChildList = ({
     } = useEditorContext();
     // Use session storage to remember the last page viewed across pages/history:
     const pageStorageKey = "child_page_" + pid;
-    const initialPage: string | null = sessionStorage.getItem(pageStorageKey);
+    const initialPage: string | null =
+        typeof sessionStorage !== "undefined" ? sessionStorage.getItem(pageStorageKey) : null;
     const [page, setPage] = useState<number>(parseInt(initialPage ?? "1"));
-    sessionStorage.setItem(pageStorageKey, page.toString());
+    if (typeof sessionStorage !== "undefined") {
+        sessionStorage.setItem(pageStorageKey, page.toString());
+    }
     const [showChildCounts, setShowChildCounts] = useState<boolean>(false);
     const [showModels, setShowModels] = useState<boolean>(false);
     const [showThumbs, setShowThumbs] = useState<boolean>(false);
